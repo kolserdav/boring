faecc7bd05907a85488848251c5a0ffd8c9f6e49
bb <blindbeat53@gmail.com>
2021-10-06 15:04:56 +0300

    switch to linux

---
+++ client/src/routes/Paths.js
@@ -0,0 +1,10 @@
+export const LOGIN_ROUTE = '/login'
+export const SIGNUP_ROUTE = '/signup'
+export const EVENTS_ROUTE = '/'
+export const EVENT_ROUTE = '/event'
+export const CONTENT_ROUTE = '/content'
+
+export const ADMIN_ROUTE = '/admin'
+export const ADMIN_EVENTS = '/admin/events'
+export const ADMIN_CONTENT = '/admin/content'
+export const ADMIN_CATEGORIES = '/admin/category'
