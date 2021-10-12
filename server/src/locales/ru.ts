677af58d9d5fc9c9cdb921b7b17bf29d38660797
kolserdav <serega12101983@gmail.com>
2021-10-12 09:35:18 +0700

    Change headers

--- server/src/locales/ru.ts
+++ server/src/locales/ru.ts
@@ -1,12 +1,12 @@
-/****************************************************************
- * Repository https://github.com/blindbeat/sweet
+/******************************************************************************************
+ * Repository: https://github.com/blindbeat/sweet
  * Author: Sergey Kolmiller
  * Email: <uyem.ru@gmail.com>
  * License: Proprietary and confidential
  * License Text: Unauthorized copying of this file, via any medium is strictly prohibited
  * Copyright: kolserdav (c), All rights reserved
- * Create date: Tue Oct 12 2021 09:18:45 GMT+0700 (Krasnoyarsk Standard Time)
-****************************************************************/
+ * Create date: Tue Oct 12 2021 09:27:02 GMT+0700 (Krasnoyarsk Standard Time)
+******************************************************************************************/
 import type { Locale } from './types';
 export const locale: Locale = {
   SERVER_ERROR: 'Ошибка сервера',
