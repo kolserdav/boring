6d75c96118878e63faaaf5374ce353b0b199692d
kolserdav <serega12101983@gmail.com>
2021-10-12 09:19:34 +0700

    Initial commit

---
+++ server/src/api/user/index.ts
@@ -0,0 +1,10 @@
+/****************************************************************
+ * Repository https://github.com/blindbeat/sweet
+ * Author: Sergey Kolmiller
+ * Email: <uyem.ru@gmail.com>
+ * License: Proprietary and confidential
+ * License Text: Unauthorized copying of this file, via any medium is strictly prohibited
+ * Copyright: kolserdav (c), All rights reserved
+ * Create date: Tue Oct 12 2021 09:18:45 GMT+0700 (Krasnoyarsk Standard Time)
+****************************************************************/
+export * as findFirst from './findFirst';
---
+++ server/src/utils/index.ts
@@ -0,0 +1,34 @@
+/****************************************************************
+ * Repository https://github.com/blindbeat/sweet
+ * Author: Sergey Kolmiller
+ * Email: <uyem.ru@gmail.com>
+ * License: Proprietary and confidential
+ * License Text: Unauthorized copying of this file, via any medium is strictly prohibited
+ * Copyright: kolserdav (c), All rights reserved
+ * Create date: Tue Oct 12 2021 09:18:45 GMT+0700 (Krasnoyarsk Standard Time)
+****************************************************************/
+import type express from 'express';
+import type * as Types from '../api/types';
+
+export const SUCCESS = 'success';
+export const ERROR = 'error';
+export const WARNING = 'warning';
+
+const { NODE_ENV }: any = process.env;
+const DEV = NODE_ENV === 'development';
+
+export function getStdErrMessage(err: Error) {
+  return DEV ? err.message : 'Standart error disabled in production';
+}
+
+export function saveLog(err: Error, req: express.Request, message: string, body: any): void {
+  if (err) {
+    console.error(new Date(), message, err, {
+      url: req.url,
+      headers: req.headers,
+      body,
+    });
+  } else {
+    console.warn(new Date(), message);
+  }
+}
